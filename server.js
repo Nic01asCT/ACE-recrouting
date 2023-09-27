f0ecfa39a725946a8a757700d23f0d6b06b2954d
Nicolas Castillo Thöni <nca135582@stud.gibb.ch>
2023-09-27 20:05:57 +0200

    added routes "/temp", "/primary" and "/number"

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const app = express()
 
 app.use(express.json())
 
+const tempRouter = require('./routes/temp')
+const primeRouter = require('./routes/prime')
+const numberRouter = require('./routes/number')
+
+app.use('/temp', tempRouter)
+app.use('/prime', primeRouter)
+app.use('/number', numberRouter)
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
